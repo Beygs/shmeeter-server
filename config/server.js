8aaaaa6bb7227a079ef37ddf7393f18b36f98d61
Beygs <boris.gilles@hotmail.fr>
2023-02-09 15:58:16 +0100

    🐛 fix: update to strapi v4

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fcb5681aebe8bf8ea32a1d10aea93d4e'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
